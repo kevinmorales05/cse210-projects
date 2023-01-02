0df9b0ba21dd3711f41083339fa7435ea8585896
Kevin Morales <kevin.morales05@epn.edu.ec>
2023-01-02 17:04:09 -0500

    Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your score: ");
+        int score = int.Parse(Console.ReadLine());
+
+        if (score > 100 || score < 0) {
+            Console.WriteLine("Please write a valid score");
+        }
+        else {
+            if (score >= 90 && score <= 100) {
+                Console.WriteLine("Your score is A");
+            }
+            else if (score >= 80 && score > 70 ) {
+                Console.WriteLine("Your score is B");
+            }
+            else if (score >= 70 && score > 60) {
+                Console.WriteLine("Your score is C");
+            }
+            else if (score >= 60 ) {
+                Console.WriteLine("Your score is D");
+            }
+            else {
+                Console.WriteLine("Your score is F");
+            }
+        }
+        
     }
 }
