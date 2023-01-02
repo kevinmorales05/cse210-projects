d080c68cdbc988573111557ad6da8b46f8373093
Kevin Morales <kevin.morales05@epn.edu.ec>
2023-01-02 18:20:51 -0500

    Prep 2, strech channel completed, implement + and - for the score

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,28 +5,58 @@ class Program
     static void Main(string[] args)
     {
         Console.Write("What is your score: ");
-        int score = int.Parse(Console.ReadLine());
+        int Score = int.Parse(Console.ReadLine());
+        string plus = " ";
+        char numString = Score.ToString()[1];
 
-        if (score > 100 || score < 0) {
-            Console.WriteLine("Please write a valid score");
+        if (Score == 100)
+        {
+            plus = "+";
         }
-        else {
-            if (score >= 90 && score <= 100) {
-                Console.WriteLine("Your score is A");
+        else if (Score == 60 || Score == 70 || Score == 80 || Score == 90) {
+            plus = "-";
+        }
+        else
+        {
+            if (numString == '7' || numString == '8' || numString == '9')
+            {
+                plus = "+";
             }
-            else if (score >= 80 && score > 70 ) {
-                Console.WriteLine("Your score is B");
+            else if (numString == '3' || numString == '2' || numString == '1')
+            {
+                plus = "-";
             }
-            else if (score >= 70 && score > 60) {
-                Console.WriteLine("Your score is C");
+
+        }
+
+        if (Score > 100 || Score < 0)
+        {
+            Console.WriteLine("Please write a valid Score");
+        }
+
+        else
+        {
+            if (Score >= 90 && Score <= 100)
+            {
+                Console.WriteLine($"Your Score is A{plus}");
             }
-            else if (score >= 60 ) {
-                Console.WriteLine("Your score is D");
+            else if (Score >= 80 && Score > 70)
+            {
+                Console.WriteLine($"Your Score is B{plus}");
             }
-            else {
-                Console.WriteLine("Your score is F");
+            else if (Score >= 70 && Score > 60)
+            {
+                Console.WriteLine($"Your Score is C{plus}");
+            }
+            else if (Score >= 60)
+            {
+                Console.WriteLine($"Your Score is D{plus}");
+            }
+            else
+            {
+                Console.WriteLine($"Your Score is F{plus}");
             }
         }
-        
+
     }
 }
