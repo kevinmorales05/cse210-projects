2ce5deef4155d399c69eecbbfe7e1273ce07d184
Kevin Morales <kevin.morales05@epn.edu.ec>
2023-01-12 16:34:58 -0500

    Prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string name = PromptUserName();
+        double number = PromptUserNumber();
+        Console.WriteLine( DisplayResult(number, name));
+    }
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    }
+    static double SquareNumber(double number)
+    {
+        return Math.Pow(number, 2);
+    }
+    static string DisplayResult(double number, string name)
+    {
+        double square = SquareNumber(number);
+        return $"{name}, the square of your number is {square.ToString()}";
     }
 }
