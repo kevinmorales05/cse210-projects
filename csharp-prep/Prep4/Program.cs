6099ee6feed91a32948a5a35ba455bc7b6e00bc8
Kevin Morales <kevin.morales05@epn.edu.ec>
2023-01-12 16:02:12 -0500

    Prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int number;
+        List<int> numbers = new List<int>();
+
+
+        do
+        {
+            Console.Write("  Enter number: ");
+            number = int.Parse(Console.ReadLine());
+            if (number != 0){
+                numbers.Add(number);
+            }
+
+        } while (number != 0);
+        int sum = 0;
+        foreach (int num in numbers){
+            sum = sum + num;
+        }
+        Console.WriteLine(" The sum is: "+ sum);
+        Console.WriteLine(" The average is: "+ sum/numbers.Count);
+        List <int>sortedValues = numbers.OrderBy(v => v)
+            .ToList();        
+            // foreach(int num in sortedValues){
+            //     Console.WriteLine(num);
+            // };
+        Console.WriteLine(" The largest number is: "+ sortedValues[sortedValues.Count-1]);
+
     }
 }
