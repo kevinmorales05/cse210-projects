89fab97db71af0278feccb7b19207edb3c1d6e27
Kevin Morales <kevin.morales05@epn.edu.ec>
2023-01-12 09:55:41 -0500

    Prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 11);
+        bool status = true;
+
+        do
+        {
+            Console.WriteLine("What is the magic number?");
+            int guess_number = int.Parse(Console.ReadLine());
+            if (guess_number == number)
+            {
+                status = false;
+                Console.WriteLine("You guessed it!");
+            }
+            else if (guess_number > number)
+            {
+                Console.WriteLine("Higher!");
+            }
+            else if (guess_number < number)
+            {
+                Console.WriteLine("Lower!");
+            }
+
+        } while (status);
+
+
     }
 }
