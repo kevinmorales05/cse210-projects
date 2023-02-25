e03f8d440254ab269a82fcb9f0a358481fbded68
Kevin Morales <kevin.morales05@epn.edu.ec>
2023-02-24 23:10:14 -0500

    W05 Prepare: Learning Activity

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -5,5 +5,14 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning03 World!");
+        Fraction frac1 = new Fraction();
+        Console.WriteLine(frac1.GetFractionString());
+        Console.WriteLine(frac1.GetDecimalValue());
+        Fraction frac2 = new Fraction(2);
+        Console.WriteLine(frac2.GetFractionString());
+        Console.WriteLine(frac2.GetDecimalValue());
+        Fraction frac3 = new Fraction(1,2);
+        Console.WriteLine(frac3.GetFractionString());
+        Console.WriteLine(frac3.GetDecimalValue());
     }
 }
