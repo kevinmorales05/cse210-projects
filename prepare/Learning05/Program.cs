e648cbd043ddb8cbc36c0dc94535992661bd6cb0
Kevin Morales <kevin.morales05@epn.edu.ec>
2023-03-09 09:25:22 -0500

    Polymorphism Learning Activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -5,5 +5,17 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning05 World!");
+        Square mySquare = new Square("red", 23);
+        Console.WriteLine(mySquare.GetColor());
+        Console.WriteLine(mySquare.GetArea());
+        Circle myCircle = new Circle("white", 10);
+        Console.WriteLine(myCircle.GetColor());
+        Console.WriteLine(myCircle.GetArea());
+        Rectangle myRectangle = new Rectangle("blue", 3, 6);
+        Console.WriteLine(myRectangle.GetColor());
+        Console.WriteLine(myRectangle.GetArea());
+
+
+
     }
 }
